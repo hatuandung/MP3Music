beaed1967b16dadf1089529884922288b09afaa8
hatuandung <ha.dung792@gmail.com>
2020-07-29 19:54:41 +0700

    init

--- app/src/main/java/com/t3h/mp3music/service/MP3Service.java
+++ app/src/main/java/com/t3h/mp3music/service/MP3Service.java
@@ -1,8 +1,11 @@
 package com.t3h.mp3music.service;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
+import android.os.Build;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
@@ -21,6 +24,15 @@ public class MP3Service extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return new MP3Binder(this);
+    }
+
+    private void pushNotify(){
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            String channelId = "MP3Channel";
+            NotificationChannel channel = new NotificationChannel(channelId, channelId, NotificationManager.IMPORTANCE_MIN);
+            manager.createNotificationChannel(channel);
+        }
     }
 
     public void setData(ArrayList<Song> songs){
